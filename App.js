f32aedbdde37d233848d8d2cc711b8bc7e1136f7
zhongyi <zhongyi@gridsum.com>
2018-08-13 18:25:29 +0800

    feat: case1， toggle pics

--- App.js
+++ App.js
@@ -6,44 +6,77 @@
  * @flow
  */
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
-
+import React, {
+    Component
+} from 'react';
+import {
+    Platform,
+    StyleSheet,
+    Text,
+    View,
+    Image
+} from 'react-native';
+import { Button, Header as NativeHeader, Icon, Title } from 'native-base';
 const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
+    ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+    android: 'Double tap R on your keyboard to reload,\n' +
+        'Shake or press menu button for dev menu',
 });
 
 type Props = {};
-export default class App extends Component<Props> {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
-    );
-  }
+const getTerrain = (styles, index, isShow) => {
+    switch (index) {
+        case 0:
+            return <Image style={[styles.terrain,  (isShow) ? styles.terrain : styles.hideTerrain]} source={require('./img/pic1.jpg')} />
+        case 1:
+            return <Image style={ (isShow) ? styles.terrain : styles.hideTerrain} source={require('./img/pic2.jpg')} />
+        case 2:
+            return <Image style={ (isShow) ? styles.terrain : styles.hideTerrain} source={require('./img/pic3.jpg')} />
+        case 3:
+            return <Image style={ (isShow) ? styles.terrain : styles.hideTerrain} ource={require('./img/pic1.jpg')} />
+    }
+}
+
+export default class App extends Component < Props > {
+      state = {
+        isShow: true
+      }
+      onToggleTerrain = () =>{
+          this.setState({
+                isShow: !this.state.isShow,
+        });
+      }
+      render() {
+          return (
+              <View style={styles.container}>
+                  <Button text="fdsfdsfsdf"  onPress={this.onToggleTerrain}>
+                        <Text style={styles.btnText}>togglePic</Text>
+                  </Button>
+                  {getTerrain(styles, 0, this.state.isShow)}
+              </View>
+          )
+      }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
+    welcome: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+    },
+    terrain: {
+        width: 30,
+        height: 30,
+        position: "absolute",
+        zIndex: 2,
+    },
+    hideTerrain: {
+        opacity: 0
+    }
 });
